5d5e5896598c39cf547f4d175d316d76c7a50206
danielannaheim <danielannaheim@gmx.ch>
2017-10-22 14:02:48 +0200

    table removed

--- lernkarten.sql
+++ lernkarten.sql
@@ -17,28 +17,3 @@ CREATE TABLE IF NOT EXISTS `lernkarten` (
   `image` blob,
   PRIMARY KEY (id)
 );
-
-CREATE TABLE IF NOT EXISTS `log` (
-  `id` SMALLINT NOT NULL AUTO_INCREMENT,
-  `timestamp` text NOT NULL,
-  `ip` text NOT NULL,
-  `proxy` text NOT NULL,
-  `query` text NOT NULL,
-  PRIMARY KEY (id)
-);
-
-
-CREATE TABLE IF NOT EXISTS `test` (
-	  `id` SMALLINT NOT NULL AUTO_INCREMENT,
-	  `datum` DATE NOT NULL UNIQUE,
-	  `morgen` text NOT NULL,
-	  `abends` text NOT NULL,
-	  PRIMARY KEY (id)
-	);
-
-INSERT INTO `test` (`datum`, `morgen`, `abends`) VALUES
-	('2017-7-19', 'Hauswart Telli', '1'),
-	('2017-7-20', '2', 'Hauswart Telli'),
-	('2017-7-21', 'Hauswart Telli', '3'),
-	('2017-7-22', 'Hauswart Telli', '4');
-
