e474c1450a39a9b6644b0197540f826a51ed7f39
danielannaheim <danielannaheim@gmx.ch>
2017-10-23 15:15:57 +0200

    save php --- funzt net

--- save.php
+++ save.php
@@ -3,7 +3,28 @@
 require_once("dbcontroller.php");
 $db = new DBController();
 
-$query="INSERT INTO lernkarten (question, answer) VALUES (\"".$_GET["question"]."\", \"".$_GET["answer"]."\");";
+
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $question = test_input($_POST["question"]);
+  $answer = test_input($_POST["answer"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+
+
+
+
+
+
+$query="INSERT INTO lernkarten (question, answer) VALUES ('$question', '$answer');";
 //INSERT INTO lernkarten (question, answer) VALUES ("question", "answer");
 echo $query;
 
